2ebfb744d472a6e1b0cff231428545a18a1c9b4e
PapiAiron <cantal2227@gmail.com>
2025-12-04 23:42:23 +0800

    Debug

--- backend/app.py
+++ backend/app.py
@@ -1983,4 +1983,4 @@ def sku_pricing():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False, ssl_context='adhoc')
